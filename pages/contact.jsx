94a803290f35338365687e08a2e453d3cd112900
Primpunyaporn <117614438+Primpunyaporn@users.noreply.github.com>
2022-12-06 10:38:52 +0700

    prim

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Head from "next/head";
 
-
 export default function ContactPages() {
   return (
     <>
@@ -11,7 +10,26 @@ export default function ContactPages() {
         <link rel="icon" href="/images/logo.png" />
       </Head>
 
-      <iframe className="w-full aspect-video"  src="https://www.youtube.com/embed/dQw4w9WgXcQ" title="Rick Astley - Never Gonna Give You Up (Official Music Video)" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <div>
+        <div className=" mx-5 py-5 grid ">
+          <div className="items-center">
+            {/* <img className="mx-auto" src="/images/1.png" width="40%" /> */}
+          </div>
+
+          <div className="font-charmonman ">
+            <h1 className=" py-3 text-5xl  text-yellow-400  text-center">
+              ช่องทางติดต่อ
+            </h1>
+            <p className=" py-3 text-3xl  text-yellow-400 text-left">
+               ที่ตั้ง: บรืษัท ซูกาเวีย จำกัด 299 หมู่ 8 ต.หนองสาหร่าย อ.ปากช่อง จ.นครราชสีมา 30130 <br/>
+               โทรศัพท์: 089-999-9999 <br/>
+               อีเมล์: Stevia_Sysup@gmail.com <br/>
+               facebook: T-Active Stevia Sysup <br/>
+               Line: @T_Active
+            </p>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
