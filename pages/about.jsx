b3e99233554ba17d0d0fc5eb234f525b4b24b3e1
mind2421 <thutiya.ch@gmail.com>
2022-12-07 13:55:24 +0700

    mind

--- pages/about.jsx
+++ pages/about.jsx
@@ -10,30 +10,67 @@ export default function AboutPages() {
         <meta name="description" content="I2AROBOT 2" />
         <link rel="icon" href="/images/logo.png" />
       </Head>
-      <div>
-        <div className=" mx-5 py-5 grid grid-cols-2 ">
-          <div className="items-center">
-            <img className="mx-auto" src="/images/1.png" width="40%" />
+      <div className="container px-6 py-12 mx-auto">
+        <div className=" bg-[#FCFAF1] mt-4 mx-5 py-5 md:grid md:grid-cols-2 rounded-xl">
+          <div className=" my-auto ">
+            <img className="mx-auto" src="/images/5.png" width="10%" />
           </div>
 
-          <div className="font-charmonman ">
-            <h1 className=" py-3 text-5xl  text-yellow-400  text-center">
-              น้ำหญ้าหวานกลิ่นมะลิ
-            </h1>
-            <p className=" py-3 text-3xl  text-yellow-400 text-center">
-              หญ้าหวานทุกรูปแบบ ซูกาเวีย พิถีพิถันทุกขั้นตอน ปลูก สกัด บรรจุ
-              โดยทีมบุคคลกรคุณภาพและผู้เชี่ยวชาญ ปลอดภัยสำหรับผู้บริโภค
-              ไม่ก่อให้เกิดพิษใดๆต่อร่างกาย จากหลากสาขาจึงมั่นใจว่า
-              ผลิตภัณฑ์ซูกาเวีย
+          <div className=" font-helvethaica text-[#FFD700] text-center ">
+            <h1 className=" py-3 text-5xl  ">น้ำหญ้าหวานกลิ่นมะลิ</h1>
+            <p className=" py-3 text-3xl">
+              All varieties of stevia, Sugavia, are meticulously grown,
+              harvested, and packed by a team of specialists and professionals.
+              from many branches, guaranteeing the customer safety of Sugavia
+              products.doesn't have any harmful effects on the body
             </p>
           </div>
         </div>
 
+        <div className=" bg-[#FCFAF1] mt-4 mx-5 py-5 md:grid md:grid-cols-2  rounded-xl">
+          <div className=" w-auto  md:hidden  my-auto">
+            <img className="mx-auto" src="/images/5.png" width="10%" />
+          </div>
 
+          <div className="   font-helvethaica text-[#FFD700] text-center ">
+            <h1 className=" py-3 text-5xl  ">น้ำหญ้าหวานกลิ่น Originals</h1>
+            <p className=" py-3 text-3xl text-center">
+              Stevia herbs help increase vitality. Allows more blood to feed the
+              brain Help in the treatment of patients with diabetes. blood sugar
+              levels are Suitable for those who want to control blood sugar
+              levels and help reduce high blood fat. Reduces the risk of heart
+              disease high blood pressure and obesity, nourishes the liver, and
+              heals both internal and external wounds
+            </p>
+          </div>
 
+          <div className="w-0 md:w-auto  my-auto">
+            <img className="mx-auto" src="/images/5.png" width="10%" />
+          </div>
+        </div>
 
+        <div className="bg-[#FCFAF1] mt-4 mx-5 py-5 md:grid md:grid-cols-2 rounded-xl">
+          <div className="my-auto">
+            <img className="mx-auto" src="/images/5.png" width="10%" />
+          </div>
 
-
+          <div className=" font-helvethaica text-[#FFD700] text-center ">
+            <p className=" py-3 text-base  md:text-xl lg:text-1xl  ">
+              Stevia is a plant that has been used by humans for over 1,500
+              years. It was discovered and used for the first time by indigenous
+              peoples of South America. Humans have consumed stevia extracts as
+              an ingredient in well-brewed tea, as well as in ancient herbal
+              medicine. Who bought the original stevia, which Paraguayans call
+              kar-he-e or yerGa in Spanish Judle as a "sweet herb," is a herb
+              used by the people of Paraguay and Brazil to sweeten food and
+              drinks. And has been brewed as a drinking tea called (Mate' for
+              over 400 years. It was discovered that Japan was the first country
+              in Asia to widely use stevia extract, which is used as a component
+              of various foods and beverages) such as pickled vegetables, soy
+              sauce, soya bean paste, minced fish meat, and so on.
+            </p>
+          </div>
+        </div>
       </div>
     </>
   );
--- pages/index.jsx
+++ pages/index.jsx
@@ -10,76 +10,73 @@ export default function Home() {
       </Head>
 
       <div className="container px-6 py-12 mx-auto">
-        <div className=" mx-5 py-5 md:grid md:grid-cols-2 ">
+
+        <div className=" bg-[#FCFAF1] mx-1 py-1 md:grid md:grid-cols-2 rounded-xl ">
+
           <div className="my-auto">
             <img className="mx-auto" src="/images/1.png" width="50%" />
           </div>
 
-          <div className=" font-trajan  py-3 md:flex md:flex-col md:justify-center text-yellow-500 text-center ">
-            <h1 className="  text-2xl md:text-4xl lg:text-5xl  ">
-              STEVIA
-            </h1>
-            <p className=" text-base  md:text-xl lg:text-1xl ">
+          <div className=" font-helvethaica  py-3 md:flex md:flex-col md:justify-center text-yellow-500 text-center ">
+
+            <h1 className="  text-2xl md:text-4xl lg:text-5xl ">STEVIA</h1>
+
+            <p className=" text-base  md:text-2xl lg:text-4xl ">
               All varieties of stevia, Sugavia, are meticulously grown,
               harvested, and packed by a team of specialists and professionals.
               from many branches, guaranteeing the customer safety of Sugavia
-              products.
+              products.doesn't have any harmful effects on the body
             </p>
-            <p className=" text-xl">doesn't have any harmful effects on the body</p>
           </div>
         </div>
 
         <hr class="my-6 border-yellow-500 md:my-8 dark:border-yellow-700" />
 
-        <div className=" mx-5 py-5 grid lg:grid-cols-2">
-          <div className=" mx-5 py-5 md:grid md:grid-cols-2 ">
-
+        <div className="  mx-5 py-5 grid lg:grid-cols-2">
+          <div className="bg-[#FCFAF1] mx-5 py-5 md:grid md:grid-cols-2 rounded-xl ">
             <div className="my-auto">
-              <img className="mx-auto" src="/images/img01.jpg" width="100%" />
+              <img className="mx-auto rounded-xl" src="/images/img01.jpg" width="70%" />
             </div>
 
-            <div className="py-3 md:flex md:flex-col md:justify-center text-yellow-500 ">
-              <h1 className="font-trajan text-3xl md:text-5xl text-center ">
-              Product Features
+            <div className=" font-helvethaica py-3 text-center  md:flex md:flex-col md:justify-center text-yellow-500 ">
+              <h1 className="text-2xl md:text-4xl lg:text-5xl  ">
+                Product Features 
               </h1>
-              <p className="   text-2xl  md:text-3xl  ">
-                หญ้าหวานทุกรูปแบบ ซูกาเวีย พิถีพิถันทุกขั้นตอน ปลูก สกัด บรรจุ
-                โดยทีมบุคคลกรคุณภาพและผู้เชี่ยวชาญ ปลอดภัยสำหรับผู้บริโภค
-                ไม่ก่อให้เกิดพิษใดๆต่อร่างกาย จากหลากสาขาจึงมั่นใจว่า
-                ผลิตภัณฑ์ซูกาเวีย
+              <p className=" text-base  md:text-1xl lg:text-2xl">
+                Stevia herbs help increase vitality. Allows more blood to feed
+                the brain Help in the treatment of patients with diabetes. blood
+                sugar levels are Suitable for those who want to control blood
+                sugar levels and help reduce high blood fat. Reduces the risk of
+                heart disease high blood pressure and obesity, nourishes the
+                liver, and heals both internal and external wounds
               </p>
             </div>
           </div>
 
-
-          <div className=" mx-5 py-5 md:grid md:grid-cols-2 ">
-
-          <div className="w-auto  md:hidden  my-auto">
-              <img className="mx-auto" src="/images/7.png" width="100%" />
+          <div className=" bg-[#FCFAF1] mx-5 py-5 md:grid md:grid-cols-2 rounded-xl ">
+            <div className="w-auto  md:hidden  my-auto">
+              <img className="mx-auto rounded-xl" src="/images/7.png" width="70%" />
             </div>
 
-            <div className="py-3 md:flex md:flex-col md:justify-center text-yellow-500 ">
-              <h1 className="py-3 text-3xl md:text-5xl text-center ">
-                โรงงานบรรจุขวด
+            <div className=" font-helvethaica py-3 text-center  md:flex md:flex-col md:justify-center  text-yellow-500 ">
+              <h1 className="py-3 text-3xl md:text-5xl ">
+                bottling factory
               </h1>
-              <p className="  py-3 text-2xl  md:text-3xl ">
-                หญ้าหวานทุกรูปแบบ ซูกาเวีย พิถีพิถันทุกขั้นตอน ปลูก สกัด บรรจุ
-                โดยทีมบุคคลกรคุณภาพและผู้เชี่ยวชาญ ปลอดภัยสำหรับผู้บริโภค
-                ไม่ก่อให้เกิดพิษใดๆต่อร่างกาย จากหลากสาขาจึงมั่นใจว่า
-                ผลิตภัณฑ์ซูกาเวีย
+              <p className=" py-3 text-2xl  md:text-3xl ">
+                our packaging Make use of clean equipment. which we have kept in
+                high security Contains stevia extract in a bottle, handy, as
+                well as easy to use
+                FDA certification, GMP, notification receipt number 30-1-6300020597
               </p>
             </div>
 
             <div className="w-0 md:w-auto  my-auto">
-              <img className="mx-auto" src="/images/7.png" width="100%" />
+              <img className="mx-auto rounded-xl" src="/images/7.png" width="70%" />
             </div>
           </div>
         </div>
 
         <div>
-          <div></div>
-
-          <div></div>
         </div>
       </div>
     </>
