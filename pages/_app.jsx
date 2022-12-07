94b0156e901028b2cbf8989bc7501f06f661d0d3
mind2421 <thutiya.ch@gmail.com>
2022-12-07 16:45:53 +0700

    footer

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,6 +1,5 @@
 import Layout from "../components/Layout/Layout";
 import "../styles/globals.scss";
-import "react-datepicker/dist/react-datepicker.css";
 
 function MyApp({ Component, pageProps }) {
   return (
