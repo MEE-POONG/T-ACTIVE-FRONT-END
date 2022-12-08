e1743e39df4cbf95196032ae9596ea24106c8eb1
mind2421 <thutiya.ch@gmail.com>
2022-12-08 10:36:26 +0700

    mind

--- pages/about.jsx
+++ pages/about.jsx
@@ -10,14 +10,20 @@ export default function AboutPages() {
         <meta name="description" content="I2AROBOT 2" />
         <link rel="icon" href="/images/logo.png" />
       </Head>
-      <div className="container px-6 py-12 mx-auto">
-        <div className=" bg-[#FCFAF1] mt-4 mx-5 py-5 md:grid md:grid-cols-2 rounded-xl">
-          <div className=" my-auto ">
-            <img className="mx-auto" src="/images/5.png" width="10%" />
+
+      <div className=" my-auto ">
+            <img className="mx-auto" src="/images/A_1.png" width="30%" />
           </div>
 
+      <div className="container px-6 py-12 mx-auto">
+        <div className="  mt-4 mx-5 py-5 md:grid md:grid-cols-2 rounded-xl">
+
+        <div className=" my-auto ">
+            <img className="mx-auto" src="/images/1.png" width="30%" />
+          </div>
+
+
           <div className=" font-helvethaica text-[#FFD700] text-center ">
-            <h1 className=" py-3 text-5xl  ">น้ำหญ้าหวานกลิ่นมะลิ</h1>
             <p className=" py-3 text-3xl">
               All varieties of stevia, Sugavia, are meticulously grown,
               harvested, and packed by a team of specialists and professionals.
@@ -27,35 +33,15 @@ export default function AboutPages() {
           </div>
         </div>
 
-        <div className=" bg-[#FCFAF1] mt-4 mx-5 py-5 md:grid md:grid-cols-2  rounded-xl">
-          <div className=" w-auto  md:hidden  my-auto">
-            <img className="mx-auto" src="/images/5.png" width="10%" />
-          </div>
+        <hr class="my-6 border-yellow-500 md:my-8 dark:border-yellow-700" />
 
-          <div className="   font-helvethaica text-[#FFD700] text-center ">
-            <h1 className=" py-3 text-5xl  ">น้ำหญ้าหวานกลิ่น Originals</h1>
-            <p className=" py-3 text-3xl text-center">
-              Stevia herbs help increase vitality. Allows more blood to feed the
-              brain Help in the treatment of patients with diabetes. blood sugar
-              levels are Suitable for those who want to control blood sugar
-              levels and help reduce high blood fat. Reduces the risk of heart
-              disease high blood pressure and obesity, nourishes the liver, and
-              heals both internal and external wounds
-            </p>
-          </div>
-
-          <div className="w-0 md:w-auto  my-auto">
-            <img className="mx-auto" src="/images/5.png" width="10%" />
-          </div>
-        </div>
-
-        <div className="bg-[#FCFAF1] mt-4 mx-5 py-5 md:grid md:grid-cols-2 rounded-xl">
+        <div className="mt-4 mx-5 py-5 md:grid md:grid-cols-2 rounded-xl">
           <div className="my-auto">
-            <img className="mx-auto" src="/images/5.png" width="10%" />
+            <img className="mx-auto" src="/images/A_2.jpg" width="55%" />
           </div>
 
           <div className=" font-helvethaica text-[#FFD700] text-center ">
-            <p className=" py-3 text-base  md:text-xl lg:text-1xl  ">
+            <p className="my-auto text-base  md:text-xl lg:text-1xl  ">
               Stevia is a plant that has been used by humans for over 1,500
               years. It was discovered and used for the first time by indigenous
               peoples of South America. Humans have consumed stevia extracts as
@@ -72,6 +58,7 @@ export default function AboutPages() {
           </div>
         </div>
       </div>
+   
     </>
   );
 }
