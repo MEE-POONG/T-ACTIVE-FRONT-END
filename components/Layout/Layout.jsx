e5354752847aad2823324f8e9d0d0fa36c5003d6
mind2421 <thutiya.ch@gmail.com>
2022-12-09 10:29:51 +0700

    mind

--- components/Layout/Layout.jsx
+++ components/Layout/Layout.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Footer from '../Footer/Footer'
 import Navbar from '../Navbar/Navbar'
+import Slid from '../Slid/Slid'
 
 export default function Layout({children}) {
   return (
