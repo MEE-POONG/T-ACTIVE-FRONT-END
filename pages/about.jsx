d3b604d7c1cc762c828e8a6a1dc0aa633da35777
Primpunyaporn <117614438+Primpunyaporn@users.noreply.github.com>
2022-12-09 11:23:25 +0700

    prim

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Head from "next/head";
+import useAxios from "axios-hooks";
 
 
 export default function AboutPages() {
 
-  // const [{data: AboutData, loading, error}, getAbout] = useAxios({url: '/api/about'})
+  const [{data: AboutData, loading, error}, getAbout] = useAxios({url: '/api/about'})
 
   return (
     <>
@@ -18,22 +19,19 @@ export default function AboutPages() {
             <img className="mx-auto" src="/images/A_1.png" width="30%" />
           </div>
 
-      <div className="container px-6 py-12 mx-auto">
+      
+          {AboutData?.map((about, index) => (
+        <div className="container px-6 py-12 mx-auto" key={index}>
         <div className="  mt-4 mx-5 py-5 md:grid md:grid-cols-2 rounded-xl">
 
         <div className=" my-auto ">
-            <img className="mx-auto" src="/images/1.png" width="30%" />
+        <img className="mx-auto" src={about.imagea1} width="50%" />
           </div>
 
-
           <div className=" font-helvethaica text-[#FFD700] text-center ">
-            <h1 className=" py-3 text-5xl  ">น้ำหญ้าหวานกลิ่นมะลิ</h1>
-            <p className=" py-3 text-3xl">
-              All varieties of stevia, Sugavia, are meticulously grown,
-              harvested, and packed by a team of specialists and professionals.
-              from many branches, guaranteeing the customer safety of Sugavia
-              products.doesn't have any harmful effects on the body
-            </p>
+            <h1 className=" animate-charcter text-center text-6xl ">{about.title1}</h1>
+            <p className=" subtitles text-center text-3xl "> {about.subtitle1} </p>
+            <p className=" subtitles text-center text-3xl"> {about.detail1} </p>
           </div>
         </div>
 
@@ -41,23 +39,16 @@ export default function AboutPages() {
 
         <div className=" mt-4 mx-5 py-5 md:grid md:grid-cols-2  rounded-xl">
           <div className=" w-auto  md:hidden  my-auto">
-            <img className="mx-auto" src="/images/5.png" width="10%" />
           </div>
 
           <div className="font-helvethaica text-[#FFD700] text-center ">
-            <h1 className=" py-3 text-5xl  ">น้ำหญ้าหวานกลิ่น Originals</h1>
-            <p className=" py-3 text-3xl text-center">
-              Stevia herbs help increase vitality. Allows more blood to feed the
-              brain Help in the treatment of patients with diabetes. blood sugar
-              levels are Suitable for those who want to control blood sugar
-              levels and help reduce high blood fat. Reduces the risk of heart
-              disease high blood pressure and obesity, nourishes the liver, and
-              heals both internal and external wounds
-            </p>
+          <h1 className=" animate-charcter text-center text-6xl ">{about.title2}</h1>
+            <p className=" subtitles text-center text-3xl"> {about.subtitle2} </p>
+            <p className=" subtitles text-center text-3xl"> {about.detail2} </p>
           </div>
 
           <div className="w-0 md:w-auto  my-auto">
-            <img className="mx-auto" src="/images/5.png" width="10%" />
+          <img className="mx-auto" src={about.imagea2} width="50%" />
           </div>
         </div>
 
@@ -65,29 +56,18 @@ export default function AboutPages() {
 
         <div className="mt-4 mx-5 py-5 md:grid md:grid-cols-2 rounded-xl">
           <div className="my-auto">
-            <img className="mx-auto" src="/images/5.png" width="10%" />
+          <img className="mx-auto" src={about.imagea3} width="50%" />
           </div>
 
           <div className=" font-helvethaica text-[#FFD700] text-center ">
-            <p className="my-auto text-base  md:text-xl lg:text-1xl  ">
-              Stevia is a plant that has been used by humans for over 1,500
-              years. It was discovered and used for the first time by indigenous
-              peoples of South America. Humans have consumed stevia extracts as
-              an ingredient in well-brewed tea, as well as in ancient herbal
-              medicine. Who bought the original stevia, which Paraguayans call
-              kar-he-e or yerGa in Spanish Judle as a "sweet herb," is a herb
-              used by the people of Paraguay and Brazil to sweeten food and
-              drinks. And has been brewed as a drinking tea called (Mate' for
-              over 400 years. It was discovered that Japan was the first country
-              in Asia to widely use stevia extract, which is used as a component
-              of various foods and beverages) such as pickled vegetables, soy
-              sauce, soya bean paste, minced fish meat, and so on.
-            </p>
+          <h1 className="animate-charcter text-center text-6xl ">{about.title3}</h1>
+            <p className=" subtitles text-center text-3xl "> {about.subtitle3} </p>
+            <p className=" subtitles text-center text-3xl "> {about.detail3} </p>
           </div>
-          
+
         </div>
       </div>
-   
+        ))}
     </>
   );
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -2,9 +2,7 @@ import useAxios from "axios-hooks";
 import Head from "next/head";
 
 export default function Home() {
-  const [{ data: homeeData, loading, error }, getHomee] = useAxios({
-    url: "/api/homee",
-  });
+  const [{ data: homeeData, loading, error }, getHomee] = useAxios({ url: "/api/homee", });
 
   return (
     <>
@@ -14,82 +12,51 @@ export default function Home() {
         <link rel="icon" href="/images/logo.png" />
       </Head>
 
+     
       {homeeData?.map((homee, index) => (
-        <div className="container px-6 py-12 mx-auto " key={index}>
-          <div className=" mx-1 py-1 md:grid md:grid-cols-2 rounded-xl ">
-            <div className="my-auto">
-              <img className="mx-auto" src={homee.imageh} width="50%" />
-            </div>
+        <div className="container px-6 py-12 mx-auto" key={index}>
+        <div className="  mt-4 mx-5 py-5 md:grid md:grid-cols-2 rounded-xl">
 
-            <div className=" font-helvethaica  py-3 md:flex md:flex-col md:justify-center text-center rounded-xl text-transparent bg-clip-text bg-gradient-to-r from-[#ceaf00] to-[#ffff09]">
-              <h1 className="  text-2xl md:text-4xl lg:text-5xl "> {homee.title}</h1>
-            </div>
-            <p className=" text-base  md:text-2xl lg:text-4xl"> {homee.detail} </p>
+        <div className=" my-auto ">
+        <img className="mx-auto" src={homee.imagea1} width="50%" />
           </div>
 
-          <hr class="my-6 border-yellow-500 md:my-8" />
+          <div className=" font-helvethaica text-[#FFD700] text-center ">
+            <h1 className=" animate-charcter text-center text-6xl ">{homee.title1}</h1>
+            <p className=" subtitles text-center text-3xl "> {homee.subtitle1} </p>
+            <p className=" subtitles text-center text-3xl"> {homee.detail1} </p>
+          </div>
+        </div>
 
-          <div className=" mx-5 py-5 md:grid md:grid-cols-2 rounded-xl ">
-            <div className="my-auto">
-              <img className="mx-auto" src={homee.imageh} width="50%" />
-            </div>
+        <hr class="my-6 border-yellow-500 md:my-8 dark:border-yellow-700" />
 
-            <div className=" font-helvethaica py-3 text-center  md:flex md:flex-col md:justify-center  text-transparent bg-clip-text bg-gradient-to-r from-[#ceaf00] to-[#ffff09] ">
-              <h1 className="text-2xl md:text-4xl lg:text-5xl  "> {homee.title} </h1>
-              <p className=" text-base  md:text-1xl lg:text-2xl">{homee.detail}</p>
-            </div>
+        <div className=" mt-4 mx-5 py-5 md:grid md:grid-cols-2  rounded-xl">
+          <div className=" w-auto  md:hidden  my-auto">
           </div>
 
-          <div className=" mx-5 py-5 md:grid md:grid-cols-2 rounded-xl ">
-            <div className="w-auto  md:hidden  my-auto">
-            <img className="mx-auto" src={homee.imageh} width="50%" />
-          
-            </div>
-
-            <div className=" font-helvethaica py-3 text-center  md:flex md:flex-col md:justify-center   text-transparent bg-clip-text bg-gradient-to-r from-[#ceaf00] to-[#ffff09] ">
-              <h1 className="py-3 text-3xl md:text-5xl ">{homee.title}</h1>
-              <p className=" py-3 text-2xl  md:text-3xl ">{homee.detail} </p>
-            </div>
-
-            <div className="w-0 md:w-auto  my-auto">
-              <img
-                className="mx-auto rounded-xl"
-                src="/images/7.png"
-                width="70%"
-              />
-            </div>
+          <div className="font-helvethaica text-[#FFD700] text-center ">
+          <h1 className=" animate-charcter text-center text-6xl ">{homee.title2}</h1>
+            <p className=" subtitles text-center text-3xl"> {homee.subtitle2} </p>
+            <p className=" subtitles text-center text-3xl"> {homee.detail2} </p>
           </div>
 
-          <hr class="my-6 border-yellow-500 md:my-8" />
+          <div className="w-0 md:w-auto  my-auto">
+          <img className="mx-auto" src={homee.imagea2} width="50%" />
+          </div>
+        </div>
 
-          <div>
-            <div className=" mx-5 py-5 md:grid md:grid-cols-2 rounded-xl ">
-              <div className="w-auto  md:hidden  my-auto">
-                <img
-                  className="mx-auto rounded-xl"
-                  src="/images/7.png"
-                  width="70%"
-                />
-              </div>
+        <hr class="my-6 border-yellow-500 md:my-8 dark:border-yellow-700" />
 
-              <div className=" font-helvethaica py-3 text-center  md:flex md:flex-col md:justify-center   text-transparent bg-clip-text bg-gradient-to-r from-[#ceaf00] to-[#ffff09] ">
-                <h1 className="py-3 text-3xl md:text-5xl ">bottling factory</h1>
-                <p className=" py-3 text-2xl  md:text-3xl ">
-                  our packaging Make use of clean equipment. which we have kept
-                  in high security Contains stevia extract in a bottle, handy,
-                  as well as easy to use FDA certification, GMP, notification
-                  receipt number 30-1-6300020597
-                </p>
-              </div>
+        <div className="mt-4 mx-5 py-5 md:grid md:grid-cols-2 rounded-xl">
+          <div className="my-auto">
+          <img className="mx-auto" src={homee.imagea3} width="50%" />
+          </div>
 
-              <div className="w-0 md:w-auto  my-auto">
-                <img
-                  className="mx-auto rounded-xl"
-                  src="/images/7.png"
-                  width="70%"
-                />
-              </div>
-            </div>
+          <div className=" font-helvethaica text-[#FFD700] text-center ">
+          <h1 className="animate-charcter text-center text-6xl ">{homee.title3}</h1>
+            <p className=" subtitles text-center text-3xl "> {homee.subtitle3} </p>
+            <p className=" subtitles text-center text-3xl "> {homee.detail3} </p>
+          </div>
           </div>
         </div>
       ))}
