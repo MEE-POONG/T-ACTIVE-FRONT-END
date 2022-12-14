43ef0dda99c36a5a34bb36aeec0f4211424e6882
mind2421 <thutiya.ch@gmail.com>
2022-12-14 09:36:54 +0700

    nav-index-mind

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -1,50 +1,58 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { FiAlignJustify,FiMinus } from "react-icons/fi";
 export default function Navbar() {
+
+  const [navbar, setNavbar] = useState(false);
+
   return (
     <>
-<div className=" sticky top-0 bg-black bg-opacity-80 py-2 mx-auto ">
+<div className=" z-10">
 
-  <nav>
+<nav>
 
-  <div className="flex flex-wrap justify-between md:gap-10 md:flex-nowrap">
-
-    <div className="font-font01 text-yellow-500 flex-col items-center justify-start order-1 hidden w-full md:flex md:flex-row md:justify-end md:w-auto md:order-none md:flex-1">
-      <a href="/" className=" block py-2 pl-3 pr-4 ">Home</a>
-      <a href="/about" className="block py-2 pl-3 pr-4">About</a>
-    </div>
-
-    <a href="/" className="flex items-center">
-        <img src="/images/logo.png" className="h-16  m-auto sm:h-20" alt="T-ACTIVE Logo" />
-    </a>
-
-    <div className=" font-font01 text-yellow-500  flex-col items-center justify-start order-2 hidden w-full md:flex md:flex-row md:w-auto md:flex-1 md:order-none">
-      <a href="/products" className="block py-2 pl-3 pr-4 ">Product</a>
-      <a href="/contact" className="block py-2 pl-3 pr-4 ">Contact</a>
-    </div>
-
-    {/* <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
-      <span className="sr-only">Open main menu</span>
-      <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
-    </button>
-
-    {/* <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-      <ul className=" hidden text-[#ffffff] flex flex-col p-4 mt-4 border rounded-lg  md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0   ">
-        <li>
-          <a href="/" className="block py-2 pl-3 pr-4 ">Home</a>
-        </li>
-        <li>
-          <a href="/about" className="block py-2 pl-3 pr-4">About</a>
-        </li> 
-        <li>
-          <a href="/product" className="block py-2 pl-3 pr-4 ">Product</a>
-        </li>
-        <li>
-          <a href="/contact" className="block py-2 pl-3 pr-4 ">Contact</a>
-        </li>
-      </ul>
-    </div> */} 
+<div className="flex flex-wrap justify-between md:gap-10 md:flex-nowrap">
+  <div className=" z-10 absolute ">
+      <a href="/" className=" md:flex items-center">
+      <img src="/images/logo.png" className="  py-5 px-5 h-10  m-auto md:h-44 " alt="T-ACTIVE Logo" />
+  </a>
   </div>
+
+
+  <div className=" z-10 absolute right-0 mt-5 bg-black bg-opacity-80 rounded-3xl font-font01 px-10 py-5 text-yellow-500  flex-col items-center justify-start order-2 hidden w-full md:flex md:flex-row md:w-auto md:order-none">
+    <a href="/" className=" block py-2 pl-3 pr-4 ">Home</a>
+    <a href="/about" className="block py-2 pl-3 pr-4">About</a>
+    <a href="/products" className="block py-2 pl-3 pr-4 ">Product</a>
+    <a href="/contact" className="block py-2 pl-3 pr-4 ">Contact</a>
+  </div>
+
+
+
+  <button  id="btn" type="button" className="inline-flex items-center p-2 mr-5 text-sm text-yellow-500  rounded-lg md:hidden  focus:outline-none focus:ring-2 focus:ring-yellow-200 " onClick={() => setNavbar(!navbar)} >
+
+    {navbar ? (
+                                  <FiMinus/>
+                              ) : (
+                                  <FiAlignJustify/>
+                              )}
+  </button>
+
+  <div className={`w-full md:hidden ${ navbar ? "block" : "hidden" }`}>
+    <ul className=" static right-0  mt-2 font-font01 md:hidden text-right text-yellow-500 ">
+      <li>
+        <a href="/" className="block py-2 pl-3 pr-4">Home</a>
+      </li>
+      <li>
+        <a href="/about" className="block py-2 pl-3 pr-4">About</a>
+      </li> 
+      <li>
+        <a href="/products" className="block py-2 pl-3 pr-4 ">Product</a>
+      </li>
+      <li>
+        <a href="/contact" className="block py-2 pl-3 pr-4 ">Contact</a>
+      </li>
+    </ul>
+  </div> 
+</div>
 </nav>
 
 </div>
--- pages/index.jsx
+++ pages/index.jsx
@@ -16,10 +16,11 @@ export default function Home() {
 
       {homeeData?.map((homee, index) => (
         
-        <div className="container px-6 py-12 mx-auto" key={index}>
-          <div className="  mt-4 mx-5 py-5 md:grid md:grid-cols-2 rounded-xl">
-            <div className=" my-auto ">
-              <img className="mx-auto" src={homee.imageh1} width="50%" />
+        <div className="" key={index}>
+           
+          <div className="">
+            <div className=" w-[100vw] h-[100vh] ">
+              <img className="" style={{objectFit: "cover", backgroundSize: "cover" ,opacity: "1", width: "100%", height: "100%", position: "absolute", top: "0px", left: "0px", display: "block"}} src={"/images/bg_in.jpg"} />
             </div>
 
             <div className=" font-font01 text-[#FFD700] text-center ">
@@ -34,56 +35,7 @@ export default function Home() {
                 dangerouslySetInnerHTML={{ __html: homee?.detail1 }}
               />
             </div>
-          </div>
-
-          <hr className="my-6 border-yellow-500 md:my-8 dark:border-yellow-700" />
-
-          <div className=" mt-4 mx-5 py-5 md:grid md:grid-cols-2  rounded-xl">
-            
-          <div className=" w-auto md:hidden  my-auto">
-              <img className="mx-auto" src={homee.imageh2} width="50%" />
-            </div>
-
-            <div className=" w-auto  md:hidden  my-auto"></div>
-
-            <div className="font-font01 text-[#FFD700] text-center ">
-              <h1 className=" animate-charcter text-center text-5xl ">
-                {homee.title2}
-              </h1>
-              <div className=" font-font02 subtitles py-3 text-center text-3xl">
-                {homee.subtitle2}
-              </div>
-              <div
-                className=" font-font02 subtitles text-center text-xl md:text-2xl"
-                dangerouslySetInnerHTML={{ __html: homee?.detail2 }}
-              />
-            </div>
-
-            <div className="w-0 md:w-auto  my-auto">
-              <img className="mx-auto" src={homee.imageh2} width="50%" />
-            </div>
-          </div>
-
-          <hr className="my-6 border-yellow-500 md:my-8 dark:border-yellow-700" />
-
-          <div className="mt-4 mx-5 py-5 md:grid md:grid-cols-2 rounded-xl">
-            <div className="my-auto">
-              <img className="mx-auto" src={homee.imageh3} width="50%" />
-            </div>
-
-            <div className=" font-font01 text-[#FFD700] text-center ">
-              <h1 className="animate-charcter text-center text-5xl ">
-                {homee.title3}
-              </h1>
-              <div className=" font-font02 subtitles py-3 text-center text-3xl">
-                {homee.subtitle3}
-              </div>
-              <div
-                className="  font-font02 subtitles text-center text-xl md:text-2xl"
-                dangerouslySetInnerHTML={{ __html: homee?.detail3}}
-              />
-            </div>
-          </div>
+          </div>  
         </div>
       ))}
     </>
