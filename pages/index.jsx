bcedbae5ece029a43717bfc2d1b089e7c0081504
mind2421 <thutiya.ch@gmail.com>
2022-12-14 11:26:24 +0700

    mind

--- pages/index.jsx
+++ pages/index.jsx
@@ -18,23 +18,51 @@ export default function Home() {
         
         <div className="" key={index}>
            
-          
+          {/* BOX1 */}
             <div className=" h-screen">
               <img className=" z-0 object-cover bg-cover  opacity-100 w-full h-full absolute top-0 block" src={"/images/bg_in.jpg"} />
               <div className=" z-10 bg-black bg-opacity-20 object-cover bg-cover  opacity-100 w-full h-full absolute top-0 block "></div>
-            </div>
 
-            <div className=" font-font01 font-semibold  text-[#FFD700] text-center ">
-              <h1 className=" animate-charcter text-center text-5xl ">
-                {homee.title1}
-              </h1>
-              <div className="font-font02 subtitles py-3 text-center text-3xl">
+              <div className="  font-font01 font-semibold  text-[#FFD700] text-center ">
+
+              <div className=" z-20 absolute font-font02 bottom-32 left-16  subtitles text-center text-3xl">
                 {homee.subtitle1}
               </div>
-              <div
-                className="  font-font02 subtitles text-center text-xl md:text-2xl"
-                dangerouslySetInnerHTML={{ __html: homee?.detail1 }}
-              />
+
+              <h1 className=" animate-charcter z-20 absolute  bottom-16 left-16  text-center text-5xl ">
+                {homee.title1}
+              </h1>
+
+              <div className=" z-20 absolute bottom-14 right-20  text-center py-5">
+              <a href="https://www.facebook.com/TActiveSteviaSyrup">  <button  className="  font-font01 animate-charcter font-semibold w-20 h-10 mx-5 my-5 border-double border-4 border-yellow-300/100 rounded-xl"> BUY </button> </a>  
+              </div>
+
+            </div>
+
+            </div>
+
+
+            {/* BOX2 */}
+            <div className=" relative h-screen">
+              <img className=" z-0 object-cover bg-cover  opacity-100 w-2/4 h-2/4 absolute top-0 block" src={"/images/product/product01.jpg"} />
+              <div className=" z-10 bg-black bg-opacity-20 object-cover bg-cover  opacity-100 w-2/4 h-2/4  absolute top-0 block "></div>
+
+              <div className="  font-font01 font-semibold  text-[#FFD700] text-center ">
+
+              <div className=" z-20 absolute font-font02 bottom-32 left-16  subtitles text-center text-3xl">
+                {homee.subtitle1}
+              </div>
+
+              <h1 className=" animate-charcter z-20 absolute  bottom-16 left-16  text-center text-5xl ">
+                {homee.title1}
+              </h1>
+
+              <div className=" z-20 absolute bottom-14 right-20  text-center py-5">
+              <a href="https://www.facebook.com/TActiveSteviaSyrup">  <button  className="  font-font01 animate-charcter font-semibold w-20 h-10 mx-5 my-5 border-double border-4 border-yellow-300/100 rounded-xl"> BUY </button> </a>  
+              </div>
+
+            </div>
+
             </div>
             
         </div>
