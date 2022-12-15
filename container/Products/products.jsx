2cc78b8db03b14c1e6b98010dcf6a37666ee0fa2
Primpunyaporn <117614438+Primpunyaporn@users.noreply.github.com>
2022-12-15 10:43:24 +0700

    prim

--- container/Products/products.jsx
+++ container/Products/products.jsx
@@ -8,24 +8,18 @@ export default function Products() {
 
   return (
     <>
-{/* 
-      <div className="relative">
-      <div className="mt-20 h-screen ">
-              <img className=" z-0  object-cover bg-cover brightness-50 w-full h-full absolute top-0 block" src={"/images/product/product00.jpg"} />
-              <h1 className=" z-20 font-font01 animate-charcter font-semibold text-5xl absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"> 
-               PRODUCTS
-               </h1>
-            </div>
-            </div>
-     */}
-      
+ 
+        <h1 className="mt-20 font-font01 animate-charcter font-semibold text-center text-5xl ">PRODUCTS </h1>
+
         <div className=" container px-auto py-12 mx-auto ">
 
         <div className=" justify-items-center grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3">
+
          
         {ProductsData?.map((products, index) => (
           <div className="mx-1 my-1">
           <div className=" box1 px-16 w-96 md:container md:mx-auto" key={index}>
+
           <img className=" mx-auto py-10 px-10 w-auto h-72" src={products.imagep} />
           <div className=" md:px-6">
             <h1 className="py-4 font-font01 animate-charcter font-semibold text-center text-3xl md:text-3xl "> {products.title} </h1>
