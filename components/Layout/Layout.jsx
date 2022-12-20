96bf0fecfc1c7eca2617140814a6d097306b17c7
devilzeros <devilzeros@outlook.co.th>
2022-12-20 15:05:11 +0700

    est

--- components/Layout/Layout.jsx
+++ components/Layout/Layout.jsx
@@ -3,12 +3,28 @@ import Footer from '../Footer/Footer'
 import Navbar from '../Navbar/Navbar'
 import Slid from '../Slid/Slid'
 
-export default function Layout({children}) {
+export default function Layout({ children }) {
   return (
     <>
-    <Navbar/>
+      <div className='bg-tem '>
+        <div class="area" >
+          <ul class="circles">
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+        </div >
+      </div>
+      <Navbar />
       <main>{children}</main>
-    <Footer/>
+      <Footer />
     </>
   )
 }
