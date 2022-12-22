c17f89ec13e986427422ec10f48b4949edea66f8
mind2421 <thutiya.ch@gmail.com>
2022-12-22 09:22:46 +0700

    index

--- pages/index.jsx
+++ pages/index.jsx
@@ -37,10 +37,14 @@ export default function Home() {
                   </div>
 
                   <div className=" z-20 absolute bottom-7 right-0 md:bottom-7 md:right-16  text-center py-5 ">
-                    <a href="https://www.facebook.com/TActiveSteviaSyrup">
-                      <button className=" animate-charcter font-font01 font-semibold w-28 h-12 mx-5 my-5 border-4 border-yellow-300/100 rounded-xl">
-                        PRODUCT
-                      </button>
+
+                    <a href="https://www.facebook.com/TActiveSteviaSyrup" class="relative inline-block text-lg group">
+                      <span class="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-yellow-400 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-black ">
+                      <span class="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-black"></span>
+                      <span class="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-yellow-400 group-hover:-rotate-180 ease"></span>
+                      <span class="relative">PRODUCT</span>
+                      </span>
+                      <span class="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-yellow-400 rounded-lg group-hover:mb-0 group-hover:mr-0" data-rounded="rounded-lg"></span>
                     </a>
                   </div>
                 </div>
