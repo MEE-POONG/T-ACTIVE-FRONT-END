8f522a302de5aabf4e628fffdc390b490013b82a
mind2421 <thutiya.ch@gmail.com>
2023-01-02 21:54:41 +0700

    about

--- pages/about.jsx
+++ pages/about.jsx
@@ -12,103 +12,73 @@ export default function AboutPages() {
       <h1 className="mt-20 py-28 font-font01 animate-charcter font-semibold text-center text-5xl ">
         ABOUT{" "}
       </h1>
-
-      {AboutData?.map((about, index) => (
-        <div className=" px-10 md:px-20" key={index}>
-          <div className=" font-font01  text-center ">
-            {/* BOX1 */}
+      <div className=" px-10 md:px-20">
+      <div className=" font-font01  text-center ">
+      {AboutData?.map((about, index) => 
+        index % 2 === 0 ? (
+           
             <div className=" mt-4 mx-5 py-5 md:grid md:grid-cols-2 ">
               <div className="my-auto ">
                 <img
                   className="mx-auto drop-shadow-lg  rounded-lg w-[100%] md:w-[80%] h-[100%]"
-                  src={about.imagea1}
+                  src={about.image}
                 />
               </div>
 
               <div className=" mt-5 md:m-auto font-font01  text-center ">
                 <h1 className="animate-charcter font-semibold text-center text-2xl md:text-4xl ">
-                  {about.title1}
+                  {about.title}
                 </h1>
                 <div className=" py-3 font-font01 subtitles text-center text-[18px] md:text-[24px]">
-                  {about.subtitle1}
+                  {about.subtitle}
                 </div>
                 <div
                   className="md:mr-10 font-font01 subtitles indent-12 whitespace-normal text-justify text-sm md:text-[20px]"
-                  dangerouslySetInnerHTML={{ __html: about?.detail1 }}
+                  dangerouslySetInnerHTML={{ __html: about?.detail}}
                 />
               </div>
             </div>
+        ) : (
+            
+                    <div className=" mt-4 mx-5 py-5 md:grid md:grid-cols-2 ">
+                      <div className="  w-auto my-auto block md:hidden  ">
+                        <img
+                          className="mx-auto drop-shadow-lg rounded-lg w-[100%] md:w-[80%] h-[100%]"
+                          src={about.image}
+                          width="80%"
+                          height="100%"
+                        />
+                      </div>
 
-            {/* <hr className="my-6 border-yellow-500 md:my-8 dark:border-yellow-700" /> */}
+                      <div className=" mt-5 md:m-auto font-font01  text-center ">
+                        <h1 className=" animate-charcter font-semibold text-center text-2xl md:text-4xl ">
+                          {about.title}
+                        </h1>
+                        <div className="py-3 font-font01 subtitles text-center text-[18px] md:text-[24px]">
+                          {" "}
+                          {about.subtitle}
+                        </div>
 
-            {/* BOX2 */}
-            <div className=" mt-4 mx-5 py-5 md:grid md:grid-cols-2 ">
-              <div className=" w-auto md:hidden  my-auto ">
-                <img
-                  className="mx-auto drop-shadow-lg rounded-lg w-[100%] md:w-[80%] h-[100%]"
-                  src={about.imagea2}
-                  // width="80%"
-                  // height="100%"
-                />
-              </div>
+                        <div
+                          className=" md:ml-10 font-font01 subtitles indent-12 whitespace-normal text-justify text-sm md:text-[20px]"
+                          dangerouslySetInnerHTML={{ __html: about?.detail }}
+                        />
+                      </div>
 
-              <div className=" mt-5 md:m-auto font-font01  text-center ">
-                <h1 className=" animate-charcter font-semibold text-center text-2xl md:text-4xl ">
-                  {about.title2}
-                </h1>
-                <div className="py-3 font-font01 subtitles text-center text-[18px] md:text-[24px]">
-                  {" "}
-                  {about.subtitle2}
-                </div>
-
-                <div
-                  className=" md:ml-10 font-font01 subtitles indent-12 whitespace-normal text-justify text-sm md:text-[20px]"
-                  dangerouslySetInnerHTML={{ __html: about?.detail2 }}
-                />
-              </div>
-
-              <div className=" w-0 md:w-auto  my-auto">
-                <img
-                  className="mx-auto drop-shadow-lg rounded-lg"
-                  src={about.imagea2}
-                  width="80%"
-                  height="100%"
-                />
-              </div>
-            </div>
-
-            {/* <hr className="my-6 border-yellow-500 md:my-8 dark:border-yellow-700" /> */}
-
-            {/* BOX3 */}
-
-            <div className="  mt-4 mx-5 py-5 md:grid md:grid-cols-2 ">
-              <div className="my-auto ">
-                <img
-                  className="mx-auto drop-shadow-lg rounded-lg w-[100%] md:w-[80%] h-[100%]"
-                  src={about.imagea3}
-                  // width="80%"
-                  // height="100%"
-                />
-              </div>
-
-              <div className=" mt-5 md:m-auto font-font01  text-center ">
-                <h1 className="animate-charcter font-semibold text-center text-2xl md:text-4xl">
-                  {about.title3}
-                </h1>
-                <div className=" py-3 font-font01 subtitles text-center text-[18px] md:text-[24px]">
-                  {" "}
-                  {about.subtitle3}
-                </div>
-                <div
-                  className=" md:mr-10 font-font01 subtitles indent-12 whitespace-normal text-justify text-sm md:text-[20px]"
-                  dangerouslySetInnerHTML={{ __html: about?.detail3 }}
-                />
-              </div>
-            </div>
-          </div>
-
-          
-          {/* gallery */}
+                      <div className="my-auto w-auto hidden md:block">
+                        <img
+                          className="mx-auto drop-shadow-lg rounded-lg"
+                          src={about.image}
+                          width="80%"
+                          height="100%"
+                        />
+                      </div>
+                    </div>
+        )    
+    )} 
+      </div>
+      </div>
+      {/* gallery */}
           <div className="m-5">
             <section className="overflow-hidden text-gray-700">
               <div className="container px-5 py-2 mx-auto lg:pt-24 lg:px-32">
@@ -139,9 +109,7 @@ export default function AboutPages() {
               </div>  
             </section>
           </div>
-
-        </div>
-      ))}
+          
     </>
   );
 }
