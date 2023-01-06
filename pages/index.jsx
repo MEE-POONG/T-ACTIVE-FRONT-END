88ed3b1d6c84f2dac271ee677ec60715fe8d2c4e
mind2421 <thutiya.ch@gmail.com>
2023-01-06 15:53:32 +0700

    deBug

--- pages/index.jsx
+++ pages/index.jsx
@@ -123,11 +123,11 @@ export default function Home() {
           </div>
       </div>
 
-      {/* <div id="gallery" className="z-30 relative">
+      <div id="gallery" className="z-30 relative">
         <Gallery/>
       </div>        
     
-      <div id="About" className="z-30 relative">
+      {/* <div id="About" className="z-30 relative">
         <About  />
       </div>
 
