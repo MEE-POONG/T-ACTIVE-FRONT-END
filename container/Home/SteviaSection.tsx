cf19775c0eecf69be96624308ee9b901072e3a85
devilzeros <devilzeros@outlook.co.th>
2023-03-30 17:17:04 +0700

    est

--- container/Home/SteviaSection.tsx
+++ container/Home/SteviaSection.tsx
@@ -1,6 +1,7 @@
 import { Container, Row, Col } from 'react-bootstrap';
 import Slider from "react-slick";
 import { intro, productList } from '../../data/test';
+import Link from 'next/link';
 
 const SteviaSection = () => {
   const productIntro = intro.filter(item => item.pagelist === 'stevia');
@@ -40,34 +41,31 @@ const SteviaSection = () => {
   };
   return (
 
-    <section className="pricing-section section-padding section-bg" id="section_5" >
-      <div style={{ backgroundImage: 'url(./images/gallery/bg1.jpg)', backgroundRepeat: 'no-repeat', backgroundSize: 'cover' }}>
-      height: 1185.54px;
-    transform: translate(0px, 243.019px);
-      </div>
+    <section className="stevia-section section-padding" id="section_5" >
+
       <Container className='what-stevia'>
         <Row>
           <Col lg={8} className="mx-auto">
-            <h1 className="text-center f-s-cookie m-0">{productIntro[0]?.title}</h1>
+            <h1 className="text-center f-s-cookie m-0 f-c-gold">{productIntro[0]?.title}</h1>
             <h3 className='text-center'>{productIntro[0]?.srcipOne}</h3>
             <h3 className='text-center'>{productIntro[0]?.srcipTwo}</h3>
           </Col>
         </Row>
         <Row>
           <Col lg={4}>
-            <p>&emsp;&emsp;
+            <p className='f-c-gold'>&emsp;&emsp;
               Stevia is a natural sweetener.
               The leaves of the plant contain sweet compounds called steviol glycosides.
               which is 400 times sweeter than normal sugar
             </p>
           </Col>
           <Col lg={4}>
-            <p>&emsp;&emsp;
+            <p className='f-c-gold'>&emsp;&emsp;
               Our products are sweet, delicious, not bitter, not overpowering, and smell good.
             </p>
           </Col>
           <Col lg={4}>
-            <p>&emsp;&emsp;
+            <p className='f-c-gold'>&emsp;&emsp;
               Stevia extract can withstand heat up to 220 degrees and can be used in baking and cooking instead of sugar.
             </p>
           </Col>
@@ -77,12 +75,12 @@ const SteviaSection = () => {
             <div className="d-flex">
               <div className='text-center w-100'>
                 <img src="/images/icon/sugar.png" alt='suger-icon' />
-                <h3>
+                <h3 className='f-c-gold'>
                   <small>SUGAR</small> 0%
                 </h3>
               </div>
             </div>
-            <p className='mx-4 text-center'>
+            <p className='mx-4 text-center f-c-gold '>
               Great choice for diabetics or people who are watching their blood sugar levels.
             </p>
           </Col>
@@ -90,12 +88,12 @@ const SteviaSection = () => {
             <div className="d-flex">
               <div className='text-center w-100'>
                 <img src="/images/icon/kcal2.png" alt='suger-icon' />
-                <h3>
+                <h3 className='f-c-gold'>
                   <small>CALORIE</small> 0%
                 </h3>
               </div>
             </div>
-            <p className='mx-4 text-center'>
+            <p className='mx-4 text-center f-c-gold '>
               A good choice for those who want to reduce their calorie intake.
             </p>
           </Col>
@@ -103,34 +101,36 @@ const SteviaSection = () => {
             <div className="d-flex">
               <div className='text-center w-100'>
                 <img src="/images/icon/lowsalt.png" alt='suger-icon' />
-                <h3>
+                <h3 className='f-c-gold'>
                   <small>SODIUM</small> 0%
                 </h3>
               </div>
             </div>
-            <p className='mx-4 text-center'>
+            <p className='mx-4 text-center f-c-gold '>
               A good option for those on a low carb or ketogenic diet.
             </p>
           </Col>
           <Col lg={12}>
-            {/* <h3 className='w-100 text-center'>
-              Pros of Stevia Extract in Taste and Cooking
-            </h3>
-            <ul className="pricing-list mt-3">
-              <li className="pricing-list-item">No effect on blood sugar. </li>
-              <li className="pricing-list-item">have antioxidant properties</li>
-              <li className="pricing-list-item">help lower blood pressure.</li>
-              <li className="pricing-list-item">have antimicrobial properties</li>
-              <li className="pricing-list-item">reduce inflammation</li>
-              <li className="pricing-list-item">No effect on blood sugar. </li>
-              <li className="pricing-list-item">have antioxidant properties</li>
-              <li className="pricing-list-item">help lower blood pressure.</li>
-              <li className="pricing-list-item">have antimicrobial properties</li>
-              <li className="pricing-list-item">reduce inflammation</li>
-            </ul> */}
+            <Link className="link-fx-1 color-contrast-higher mt-4 f-c-gold" href="ticket.html">
+              <h3 className='f-c-gold'>
+                <span>Read Stevia</span>
+                <svg className="icon" viewBox="0 0 32 32" aria-hidden="true">
+                  <g fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round">
+                    <circle cx="16" cy="16" r="15.5"></circle>
+                    <line x1="10" y1="18" x2="16" y2="12"></line>
+                    <line x1="16" y1="12" x2="22" y2="18"></line>
+                  </g>
+                </svg>
+              </h3>
+            </Link>
           </Col>
         </Row>
+
       </Container>
+      <div className="video-wrap ">
+        <div style={{ backgroundImage: 'url(./images/gallery/bg1.jpg)' }} className="custom-video" />
+
+      </div>
     </section>
   );
 }
