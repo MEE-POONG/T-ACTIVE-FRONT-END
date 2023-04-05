788b80665b8558279dc12e5b94523075f6d67147
Ploy <117616170+yaminP@users.noreply.github.com>
2023-04-05 10:42:44 +0700

    about-respon

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -3,100 +3,86 @@ import LayOut from "@/components/LayOut";
 import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
 
-export default function BlogPage(){
-    return(
+export default function BlogPage() {
+    return (
         <LayOut>
             <div className="blog-page">
                 <Container>
                     <h2 className="text-center mb-5"> Work Out</h2>
-                    <Row>
-                        <Col lg={5} xs={12}>
-                            <div className="artists-thumb">
-                            <div className="artists-image-wrap">
-                                <img src="images/gallery/IMG_2292.jpg" className="blog-img img-fluid" />
-                            </div>
+                    <Row xs="auto">
+                        <Col >
+                            <div className="a-box">
+                                <div className="img-container">
+                                    <div className="img-inner">
+                                        <div className="inner-skew">
+                                            <img src="images/gallery/IMG_2292.jpg" />
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="text-container">
+                                    <h3>A blue bird</h3>
+                                    <div>
+                                        <p>
+                                            <strong>Posture:</strong>
+                                            lifting dumbbells 
+                                        </p>
+                                        <p>
+                                            <strong>Burn:</strong>
+                                            upper arm
+                                        </p>
 
-                            <div className="card-text">
-                                <p>
-                                <strong>Posture:</strong>
-                                lifting dumbbells
-                                </p>
+                                        <p>
+                                            <strong>Do:</strong>
+                                            20 s
+                                        </p>
 
-                                <p>
-                                <strong>Burn:</strong>
-                                upper arm
-                                </p>
-
-                                <p>
-                                <strong>Do:</strong>
-                                20 s
-                                </p>
-
-                                <hr />
-
-                                <p className="mb-0">
-                                <strong>Youtube Channel:</strong>
-                                <a href="https://www.youtube.com/shorts/FOxCMhexNfI">watch</a>
-                                </p>
-                            </div>
-
-                            {/* <div className="artists-hover">
-                                <p>
-                                <strong>Posture:</strong>
-                                lifting dumbbells
-                                </p>
-
-                                <p>
-                                <strong>Burn:</strong>
-                                upper arm
-                                </p>
-
-                                <p>
-                                <strong>Do:</strong>
-                                20 s
-                                </p>
-
-                                <hr />
-
-                                <p className="mb-0">
-                                <strong>Youtube Channel:</strong>
-                                <a href="https://www.youtube.com/shorts/FOxCMhexNfI">watch</a>
-                                </p>
-                            </div> */}
+                                        <hr />
+                                        <p className="mb-0">
+                                            <strong>Youtube Channel:</strong>
+                                            <a href="https://www.youtube.com/shorts/FOxCMhexNfI">watch</a>
+                                        </p>
+                                    </div>
+                                </div>
                             </div>
                         </Col>
-                        <Col lg={5} xs={12}>
-                            <div className="artists-thumb">
-                            <div className="artists-image-wrap">
-                                <img src="images/gallery/IMG_2292.jpg" className="artists-image img-fluid" />
-                            </div>
+                        <Col >
+                            <div className="a-box">
+                                <div className="img-container">
+                                    <div className="img-inner">
+                                        <div className="inner-skew">
+                                            <img src="images/gallery/IMG_2292.jpg" />
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="text-container">
+                                    <h3>A blue bird</h3>
+                                    <div>
+                                        <p>
+                                            <strong>Posture:</strong>
+                                            lifting dumbbells 
+                                        </p>
+                                        <p>
+                                            <strong>Burn:</strong>
+                                            upper arm
+                                        </p>
 
-                            {/* <div className="artists-hover">
-                                <p>
-                                <strong>Posture:</strong>
-                                lifting dumbbells
-                                </p>
+                                        <p>
+                                            <strong>Do:</strong>
+                                            20 s
+                                        </p>
 
-                                <p>
-                                <strong>Burn:</strong>
-                                upper arm
-                                </p>
-
-                                <p>
-                                <strong>Do:</strong>
-                                20 s
-                                </p>
-
-                                <hr />
-
-                                <p className="mb-0">
-                                <strong>Youtube Channel:</strong>
-                                <a href="https://www.youtube.com/shorts/FOxCMhexNfI">watch</a>
-                                </p>
-                            </div> */}
+                                        <hr />
+                                        <p className="mb-0">
+                                            <strong>Youtube Channel:</strong>
+                                            <a href="https://www.youtube.com/shorts/FOxCMhexNfI">watch</a>
+                                        </p>
+                                    </div>
+                                </div>
                             </div>
                         </Col>
+                       
                     </Row>
+
                 </Container>
             </div>
         </LayOut>
