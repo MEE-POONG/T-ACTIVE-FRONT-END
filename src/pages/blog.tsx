772e7c0ed807fcabd1fcaea361b5679d550c7fb4
Ploy <117616170+yaminP@users.noreply.github.com>
2023-04-05 09:12:21 +0700

    blog

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 import LayOut from "@/components/LayOut";
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
 
 export default function BlogPage(){
     return(
@@ -9,6 +9,94 @@ export default function BlogPage(){
             <div className="blog-page">
                 <Container>
                     <h2 className="text-center mb-5"> Work Out</h2>
+                    <Row>
+                        <Col lg={5} xs={12}>
+                            <div className="artists-thumb">
+                            <div className="artists-image-wrap">
+                                <img src="images/gallery/IMG_2292.jpg" className="blog-img img-fluid" />
+                            </div>
+
+                            <div className="card-text">
+                                <p>
+                                <strong>Posture:</strong>
+                                lifting dumbbells
+                                </p>
+
+                                <p>
+                                <strong>Burn:</strong>
+                                upper arm
+                                </p>
+
+                                <p>
+                                <strong>Do:</strong>
+                                20 s
+                                </p>
+
+                                <hr />
+
+                                <p className="mb-0">
+                                <strong>Youtube Channel:</strong>
+                                <a href="https://www.youtube.com/shorts/FOxCMhexNfI">watch</a>
+                                </p>
+                            </div>
+
+                            {/* <div className="artists-hover">
+                                <p>
+                                <strong>Posture:</strong>
+                                lifting dumbbells
+                                </p>
+
+                                <p>
+                                <strong>Burn:</strong>
+                                upper arm
+                                </p>
+
+                                <p>
+                                <strong>Do:</strong>
+                                20 s
+                                </p>
+
+                                <hr />
+
+                                <p className="mb-0">
+                                <strong>Youtube Channel:</strong>
+                                <a href="https://www.youtube.com/shorts/FOxCMhexNfI">watch</a>
+                                </p>
+                            </div> */}
+                            </div>
+                        </Col>
+                        <Col lg={5} xs={12}>
+                            <div className="artists-thumb">
+                            <div className="artists-image-wrap">
+                                <img src="images/gallery/IMG_2292.jpg" className="artists-image img-fluid" />
+                            </div>
+
+                            {/* <div className="artists-hover">
+                                <p>
+                                <strong>Posture:</strong>
+                                lifting dumbbells
+                                </p>
+
+                                <p>
+                                <strong>Burn:</strong>
+                                upper arm
+                                </p>
+
+                                <p>
+                                <strong>Do:</strong>
+                                20 s
+                                </p>
+
+                                <hr />
+
+                                <p className="mb-0">
+                                <strong>Youtube Channel:</strong>
+                                <a href="https://www.youtube.com/shorts/FOxCMhexNfI">watch</a>
+                                </p>
+                            </div> */}
+                            </div>
+                        </Col>
+                    </Row>
                 </Container>
             </div>
         </LayOut>
